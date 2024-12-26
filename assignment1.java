d2eeb4fa49314b46436122bb81b49be3d00dfa57
nurassylmuktaruly <nuratop40@gmail.com>
2024-12-26 09:50:34 +0500

    create person class

---
+++ assignment1.java
@@ -0,0 +1,11 @@
+public class assignment1 {
+    public static void main(String[] args) {
+      Person person = new Person("Nurassyl", "Mukhatruly", 18,true);
+      System.out.println(person.toString());
+    }
+}
+
+
+
+
+
